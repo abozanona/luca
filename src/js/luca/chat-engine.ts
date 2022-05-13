2255c45bfa157b85344618e2fbb29bf2a7bbe543
Mahmoud-Skafi <mahmoudskafi277@gmail.com>
2022-05-14 00:23:44 +0300

    #21 Create Chat Design, Add reactions

--- src/js/luca/chat-engine.ts
+++ src/js/luca/chat-engine.ts
@@ -2,6 +2,24 @@ import { LucaEngine } from './luca-engine';
 import { SocketEngine } from './socket-engine';
 
 export class ChatEngine {
+    reactionScreenAnimation(elem: any, bounds: any, delay: any) {
+        let bottom = 0;
+        let opacity = 1;
+        let startRight = false;
+        function frame() {
+            bottom = bottom + delay;
+            opacity = opacity - .005;
+            elem.style.right = bounds + 'px';
+            elem.style.bottom = bottom + 'px';
+            elem.style.opacity = opacity;
+
+            if (bottom > 2000) {
+                elem.remove();
+                clearInterval(id)
+            }
+        }
+        let id = setInterval(frame, delay) //every Xms
+    }
     sendMessageToRoom(socketEngine: SocketEngine, messageText: string) {
         socketEngine.sendPlayerOrder(
             'message',
@@ -13,16 +31,24 @@ export class ChatEngine {
     }
 
     addMessageBubble(messageText: string) {
-        let divChatBubble = document.createElement('div');
-        divChatBubble.classList.add('luca-chat-bubble');
-        divChatBubble.innerText = messageText;
 
-        let bubblesContainer = document.getElementById('luca-chat-container');
-        if (bubblesContainer.firstChild) {
-            bubblesContainer.insertBefore(divChatBubble, bubblesContainer.firstChild);
-        } else {
-            bubblesContainer.appendChild(divChatBubble);
-        }
+        let divChatBubble = document.createElement('div');
+        divChatBubble.classList.add('luca-message-container');
+        divChatBubble.innerHTML = `
+            <div class="luca-message-information">
+                <div class="luca-message-avatar">
+                    <img src="https://i.pinimg.com/564x/70/ab/7b/70ab7b9eb95c0543e0d1ab4cd4b3152b.jpg">
+                    <h1>Skafi ps </h1>
+                 </div>
+                <p>2h ago</p>
+            </div>
+        <div class="luca-message">
+            <p> ${messageText} </p>
+        </div>
+        `;
+
+        let bubblesContainer = document.getElementsByClassName('luca-chat-messages-container')[0];
+        bubblesContainer.appendChild(divChatBubble);
     }
 
     sendReactionToRoom(socketEngine: SocketEngine, reactionName: string) {
@@ -36,14 +62,15 @@ export class ChatEngine {
     }
 
     getRandomInteger = function (min: number, max: number): number {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
+        return Math.ceil(Math.random() * (max - min + 1)) + min;
     };
+
+    getRandomIntegerRanged = function (min: number, max: number): number {
+        return Math.ceil(Math.random() * (max - min + 1)) + min * (Math.round(Math.random()) ? 1 : -1);
+    }
 
     showReactionOnScreen(reactionName: string) {
         let _this = this;
-        var randomSpeeds = function () {
-            return _this.getRandomInteger(3000, 4000);
-        }; // The lower, the faster
         var delay = 50; // The higher, the more delay
         var startScreenPercentage = 0.03;
         var endScreenPercentage = 0.97;
@@ -53,22 +80,20 @@ export class ChatEngine {
         interval = setInterval(function () {
             let imgReactionParticle = document.createElement('img');
             imgReactionParticle.classList.add('luca-reaction-particle');
-            imgReactionParticle.src = chrome.runtime.getURL('img/reactions/' + reactionName + '.gif');
+            imgReactionParticle.src = chrome.runtime.getURL('assets/imgs/' + reactionName + '.gif');
             imgReactionParticle.dataset.reactionName = reactionName;
 
             LucaEngine.fullScreenElement.appendChild(imgReactionParticle);
-            let bounds: number = _this.getRandomInteger(
+            let bounds: number = _this.getRandomIntegerRanged(
                 LucaEngine.fullScreenElement.clientWidth * startScreenPercentage,
                 LucaEngine.fullScreenElement.clientWidth * endScreenPercentage
             );
-            (<any>window).$(imgReactionParticle).animate({ left: bounds, right: bounds }, delay, function () {
-                (<any>window).$(imgReactionParticle).animate({ top: '-100%', opacity: 0 }, randomSpeeds(), function () {
-                    (<any>window).$(imgReactionParticle).remove();
-                });
-            });
+
+            _this.reactionScreenAnimation(imgReactionParticle, bounds, _this.getRandomInteger(10, 100))
             clearInterval(interval);
         }, 1);
     }
+
 }
 
 export default ChatEngine;
