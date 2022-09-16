91ffd2d282bdc0ef51092018e5f0974902644ff8
abozanona <abozanona@gmail.com>
2022-09-16 22:53:23 +0300

    #67 Show connectivity status for player

--- src/js/luca/socket-engine.ts
+++ src/js/luca/socket-engine.ts
@@ -23,13 +23,18 @@ export class SocketEngine {
         this.isSocketStarted = true;
         let _this = this;
 
-        _this.socket = (<any>window).io(process.env.SOCKET_URL, {
+        this.socket = (<any>window).io(process.env.SOCKET_URL, {
             transports: ['websocket'],
             withCredentials: false,
             path: '/socket.io',
+            reconnectionDelay: 1000,
+            reconnectionDelayMax: 50000,
+            reconnectionAttempts: 50,
         });
 
-        _this.socket.on('join', function (message: any) {
+        this.initSockeEvents();
+
+        this.socket.on('join', function (message: any) {
             _this.addUserToChat(message.sender);
             _this.chatEngine.addActionBubble(UtilsEngine.translate('TEMPLATE_ACTION_USER_JOINED_PARTY'), message.sender);
             UtilsEngine.executeUnderDifferentTabId(message.pageId, function () {
@@ -38,7 +43,7 @@ export class SocketEngine {
             });
         });
 
-        _this.socket.on('play', function (message: any) {
+        this.socket.on('play', function (message: any) {
             _this.addUserToChat(message.sender);
             _this.chatEngine.addActionBubble(UtilsEngine.translate('TEMPLATE_ACTION_USER_PLAYED_PARTY'), message.sender);
             UtilsEngine.executeUnderDifferentTabId(message.pageId, function () {
@@ -47,7 +52,7 @@ export class SocketEngine {
             });
         });
 
-        _this.socket.on('pause', function (message: any) {
+        this.socket.on('pause', function (message: any) {
             _this.addUserToChat(message.sender);
             _this.chatEngine.addActionBubble(UtilsEngine.translate('TEMPLATE_ACTION_USER_PAUSED_PARTY'), message.sender);
             UtilsEngine.executeUnderDifferentTabId(message.pageId, function () {
@@ -56,7 +61,7 @@ export class SocketEngine {
             });
         });
 
-        _this.socket.on('seek', function (message: any) {
+        this.socket.on('seek', function (message: any) {
             let formatTime = function (sec_num: number) {
                 sec_num = Math.floor(sec_num);
                 let hours: any = Math.floor(sec_num / 3600);
@@ -75,14 +80,14 @@ export class SocketEngine {
             });
         });
 
-        _this.socket.on('message', function (message: any) {
+        this.socket.on('message', function (message: any) {
             _this.addUserToChat(message.sender);
             UtilsEngine.executeUnderDifferentTabId(message.pageId, function () {
                 _this.chatEngine.addMessageBubble(message.text, message.sender);
             });
         });
 
-        _this.socket.on('reaction', function (message: any) {
+        this.socket.on('reaction', function (message: any) {
             _this.addUserToChat(message.sender);
             _this.chatEngine.addActionBubble(UtilsEngine.translate('TEMPLATE_ACTION_USER_REACTED_ON_PARTY'), message.sender);
             UtilsEngine.executeUnderDifferentTabId(message.pageId, function () {
@@ -90,18 +95,101 @@ export class SocketEngine {
             });
         });
 
-        _this.socket.on('videoToken', function (message: any) {
+        this.socket.on('videoToken', function (message: any) {
             lucaEngine.videoToken = message.videoToken;
         });
     }
 
+    private initSockeEvents() {
+        let _this = this;
+
+        function connectSuccess() {
+            let connectionStatus: HTMLElement = document.getElementById('luca-connection-status');
+            let chatToggle: HTMLElement = document.getElementById('luca-chat-outer-toggle');
+            if (!connectionStatus) {
+                return;
+            }
+            connectionStatus.innerText = "";
+            chatToggle.classList.remove('luca-chat-outer-toggle-disconnected');
+            chatToggle.classList.remove('luca-chat-outer-toggle-reconnecting');
+            if (_this.roomId) {
+                _this.sendSocketOrder('rejoinRoom', {
+                    roomId: _this.roomId,
+                })
+            }
+        }
+        function connectDisconnect() {
+            let connectionStatus: HTMLElement = document.getElementById('luca-connection-status');
+            let chatToggle: HTMLElement = document.getElementById('luca-chat-outer-toggle');
+            if (!connectionStatus) {
+                return;
+            }
+            connectionStatus.innerText = UtilsEngine.translate('TEMPLATE_CHAT_DISCONNECTED_FROM_SERVER');
+            chatToggle.classList.add('luca-chat-outer-toggle-disconnected');
+            chatToggle.classList.remove('luca-chat-outer-toggle-reconnecting');
+        }
+        function connectError() {
+            let connectionStatus: HTMLElement = document.getElementById('luca-connection-status');
+            let chatToggle: HTMLElement = document.getElementById('luca-chat-outer-toggle');
+            if (!connectionStatus) {
+                return;
+            }
+            connectionStatus.innerText = UtilsEngine.translate('TEMPLATE_CHAT_CANT_CONNECT_TO_SERVER');
+            chatToggle.classList.add('luca-chat-outer-toggle-disconnected');
+            chatToggle.classList.remove('luca-chat-outer-toggle-reconnecting');
+        }
+        function connectInProgress() {
+            let connectionStatus: HTMLElement = document.getElementById('luca-connection-status');
+            let chatToggle: HTMLElement = document.getElementById('luca-chat-outer-toggle');
+            if (!connectionStatus) {
+                return;
+            }
+            connectionStatus.innerText = UtilsEngine.translate('TEMPLATE_CHAT_RECONNECTING');
+            chatToggle.classList.remove('luca-chat-outer-toggle-disconnected');
+            chatToggle.classList.add('luca-chat-outer-toggle-reconnecting');
+        }
+
+        //Fired upon connection to the Namespace (including a successful reconnection).
+        this.socket.on('connect', function () {
+            if (_this.socket.disconnected) {
+
+            } else {
+                connectSuccess();
+            }
+        });
+        //Fired upon a connection error
+        this.socket.on('error', function (error: any) {
+            connectError();
+        });
+        //Fired upon a successful reconnection
+        this.socket.on('reconnect', function () {
+            if (_this.socket.disconnected) {
+                connectDisconnect();
+            } else {
+                connectSuccess();
+            }
+        });
+        //Fired upon an attempt to reconnect
+        this.socket.on('reconnect_attempt', function () {
+            connectInProgress();
+        });
+        //Fired upon a reconnection attempt error
+        this.socket.on('reconnect_error', function () {
+            connectError();
+        });
+        //Fired when couldn't reconnect within reconnectionAttempts
+        this.socket.on('reconnect_failed', function () {
+            connectError();
+        });
+    }
+
     public disconnectSocket() {
-        this.socket.disconnect();
         this.isSocketStarted = false;
         this.isSocketDisconnected = true;
         this.socket = null;
         this.roomId = null;
         this.currentUsers = [];
+        this.socket.disconnect();
     }
 
     private addUserToChat(sender: UserInterface) {
